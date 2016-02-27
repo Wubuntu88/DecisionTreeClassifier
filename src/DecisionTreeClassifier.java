bb2dfe3c68c3c61ba4205a4406d4217d69bf82a3
Will <wgilles1@emich.edu>
2016-02-27 00:01:34 -0500

    I added the buildTree() and classify() methods.

--- src/DecisionTreeClassifier.java
+++ src/DecisionTreeClassifier.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -5,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Set;
 import java.util.TreeSet;
 
 
@@ -33,16 +35,36 @@ public class DecisionTreeClassifier {
 			sBuffer.append(this.label);
 			return sBuffer.toString();
 		}
+	}//end of Record class
+	
+	private class TreeNode {
+		public static final String INTERNAL = "internal";
+		public static final String LEAF = "leaf";
+		
+		private String nodeType;
+		private int attribute; //column index
+		private String labelName;
+		private TreeNode left;
+		private TreeNode right;
+		
+		public TreeNode(String type, String labelName, int attribute, TreeNode left, TreeNode right){
+			nodeType = type;
+			this.left = left;
+			this.right = right;
+			this.attribute = attribute;
+			this.labelName = labelName;
+		}
+		
+		@Override
+		public String toString() {
+			String contents = nodeType.equals(TreeNode.INTERNAL) ? "attribute: " + attribute : "label: " + labelName;
+			return "Type: " + nodeType + ", " + contents;
+		}
 	}
 	
-	public static final String BINARY = "binary";
-	public static final String CATEGORICAL = "categorical";
-	public static final String ORDINAL = "ordinal";
-	public static final String CONTINUOUS = "continuous";
-	public static final String LABEL = "label";
-	private static final TreeSet<String> attributeTypes = new TreeSet<String>(
-			Arrays.asList(BINARY, CATEGORICAL, ORDINAL, CONTINUOUS, LABEL));
-
+	/**************************************************************
+	 * MAIN METHOD
+	 **************************************************************/
 	public static void main(String[] args) {
 		
 		DecisionTreeClassifier dtc = new DecisionTreeClassifier();
@@ -51,8 +73,14 @@ public class DecisionTreeClassifier {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println(dtc.toString());
 		
+		dtc.buildTree();
+		System.out.println(dtc);
+		double trainingError = dtc.computeTrainingError();
+		System.out.println("trainingError: " + trainingError);
+		//System.out.println(dtc.root);
+		//System.out.println(dtc.root.left);
+		//System.out.println(dtc.root.right);
 	}
 	
 	private int numberOfRecords;
@@ -60,20 +88,99 @@ public class DecisionTreeClassifier {
 	private int numberOfClasses;
 
 	// list of the type of the variables in records (ordinal, continuous, etc)
-	private ArrayList<String> attributeList = new ArrayList<>();
-
 	private ArrayList<Record> records = new ArrayList<>();
-
-	// for continuous variables (key is column, value is range (array of len 2)
-	private HashMap<Integer, double[]> rangeAtColumn = new HashMap<>();
-	// for ordinal variables
-	private HashMap<Integer, HashMap<String, Double>> valsForOrdinalVarAtColumn = new HashMap<>();
-	//for binary variables
 	private HashMap<String, Integer> binaryNameToIntSymbol = new HashMap<>();
-	//for categorical variables
-	private HashMap<String, Integer> categoricalNameToIntSymbol = new HashMap<>();
 	
-	public void loadTrainingData(String fileName) throws Exception {
+	//variables pertaining to the tree
+	private TreeNode root;
+	private final int RIGHT_RECORDS_BINARY_VALUE = 0;
+	private final int LEFT_RECORDS_BINARY_VALUE = 1;
+	
+	public void buildTree(){
+		TreeSet<Integer> indicesOfRecords = new TreeSet<>();
+		for(int i = 0; i < records.size(); i++){
+			indicesOfRecords.add(i);
+		}
+		TreeSet<Integer> remainingColIndices = new TreeSet<>();
+		for(int i = 0; i < numberOfAttributes; i++){
+			remainingColIndices.add(i);
+		}
+		root = build(indicesOfRecords, remainingColIndices);
+	}
+	
+	public TreeNode build(TreeSet<Integer> indicesOfRecordsLeft, TreeSet<Integer> remainingColIndices){
+		//System.out.println("building iteration");
+		if(areRecordsSameClass(indicesOfRecordsLeft)){
+			String labelName = records.get(0).label;
+			return new TreeNode(TreeNode.LEAF, labelName, -1,null, null);
+		}else if(remainingColIndices.size() == 0){
+			String majorityLabel = majorityLabel(indicesOfRecordsLeft);
+			return new TreeNode(TreeNode.LEAF, majorityLabel, -1, null, null);
+		}else{//the real tree building
+			int bestColumnToSplitRecords = bestColIndexToSplitRecords(indicesOfRecordsLeft, remainingColIndices);
+			TreeSet<Integer> leftIndices = 
+					indicesOfRecordsWithValueAtColumn(indicesOfRecordsLeft, 
+							LEFT_RECORDS_BINARY_VALUE, bestColumnToSplitRecords);
+			TreeSet<Integer> rightIndices = 
+					indicesOfRecordsWithValueAtColumn(indicesOfRecordsLeft, 
+							RIGHT_RECORDS_BINARY_VALUE, bestColumnToSplitRecords);
+			if(leftIndices.size() == 0 || rightIndices.size() == 0){
+				String label = majorityLabel(indicesOfRecordsLeft);
+				return new TreeNode(TreeNode.LEAF, label, -1, null, null);
+			}else{//building left and right nodes
+				TreeSet<Integer> leftRemainingColIndices = new TreeSet<>(remainingColIndices);
+				TreeSet<Integer> rightRemainingColIndices = new TreeSet<>(remainingColIndices);
+				
+				leftRemainingColIndices.remove(bestColumnToSplitRecords);
+				rightRemainingColIndices.remove(bestColumnToSplitRecords);
+				
+				TreeNode left = build(leftIndices, leftRemainingColIndices);
+				TreeNode right = build(rightIndices, rightRemainingColIndices);
+				
+				TreeNode node = new TreeNode(TreeNode.INTERNAL, null, bestColumnToSplitRecords, left, right);
+				return node;
+			}
+		}
+	}
+	
+	private String majorityLabel(TreeSet<Integer> indicesOfRecordsLeft) {
+		HashMap<String, Integer> frequencyOfLabels = new HashMap<>();
+		for(Integer indexOfRecord: indicesOfRecordsLeft){
+			String labelOfRecord = records.get(indexOfRecord).label;
+			if(frequencyOfLabels.containsKey(labelOfRecord)){
+				frequencyOfLabels.put(labelOfRecord, frequencyOfLabels.get(labelOfRecord) + 1);
+			}else{//if the key is not in the hashmap yet
+				frequencyOfLabels.put(labelOfRecord, 1);
+			}
+		}
+		String maxLabel = null;
+		int maxFrequency = Integer.MIN_VALUE;
+		for(String label: frequencyOfLabels.keySet()){
+			int frequencyOfCurrentLabel = frequencyOfLabels.get(label);
+			if(frequencyOfLabels.get(label) > maxFrequency){
+				maxFrequency = frequencyOfCurrentLabel;
+				maxLabel = label;
+			}
+		}
+		return maxLabel;
+	}
+
+	private boolean areRecordsSameClass(TreeSet<Integer> indicesOfRecords){
+		if(indicesOfRecords.size() == 0){
+			return false;
+		}
+		String classOfRecord = records.get(indicesOfRecords.last()).label;
+		for(Integer indexOfRecord: indicesOfRecords){
+			Record record = records.get(indexOfRecord);
+			if(record.label.equals(classOfRecord) == false){
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	
+	public void loadTrainingData(String fileName) throws IOException {
 		String whitespace = "[ ]+";
 		List<String> lines = Files.readAllLines(Paths.get(fileName),
 				Charset.defaultCharset());
@@ -86,65 +193,20 @@ public class DecisionTreeClassifier {
 
 		// second line reading the attribute types
 		String[] componentsOfThirdLine = lines.get(1).split(whitespace);
-		for (String attrType : componentsOfThirdLine) {
-			if (DecisionTreeClassifier.attributeTypes
-					.contains(attrType) == true) {
-				this.attributeList.add(attrType);
-			} else {
-				throw new Exception(
-						"attribute in file not one of the correct attributes");
-			}
-		}
 		//for binary variables
 		int binaryVariableCounter = 0;
-		//for categorical variables
-		int categoricalVariableCounter = 0;
 		
 		// third line reading the ranges of the attribute types
 		// uses rangeAtColumn hash map
 		String[] listOfRanges = lines.get(2).split(whitespace);
 		for (int colIndex = 0; colIndex < listOfRanges.length - 1; colIndex++) {
-			// range symbols are low to high
 			String[] strRange = listOfRanges[colIndex].split(",");
-			double[] range = new double[strRange.length];
-			String typeOfAttrAtIndex = this.attributeList.get(colIndex);
-			switch (typeOfAttrAtIndex) {
-			case ORDINAL:
-				// range symbols are low to high
-				int index = 0;
-				for (String symbol : strRange) {
-					range[index] = (double)index / (strRange.length - 1);
-					if (this.valsForOrdinalVarAtColumn.containsKey(colIndex)) {
-						HashMap<String, Double> map = this.valsForOrdinalVarAtColumn
-								.get(colIndex);
-						map.put(symbol, range[index]);
-					} else {// create the hash map
-						HashMap<String, Double> map = new HashMap<>();
-						map.put(symbol, range[index]);
-						this.valsForOrdinalVarAtColumn.put(colIndex, map);
-					}
-					index++;
-				}
-				this.rangeAtColumn.put(colIndex, range);
-				break;
-			case CONTINUOUS:
-				range[0] = Double.parseDouble(strRange[0]);
-				range[1] = Double.parseDouble(strRange[1]);
-				this.rangeAtColumn.put(colIndex, range);
-				break;
-			case BINARY:
-				for(String binaryName: strRange){
+			//String typeOfAttrAtIndex = this.attributeList.get(colIndex);
+			for(String binaryName: strRange){
 					binaryNameToIntSymbol.put(binaryName, binaryVariableCounter);
 					binaryVariableCounter++;
-				}
-				break;// do later because first file doesn't have binary
-			case CATEGORICAL:
-				for(String categoricalName: strRange){
-					categoricalNameToIntSymbol.put(categoricalName, categoricalVariableCounter);
-					categoricalVariableCounter++;
-				}
-				break;
 			}
+			binaryVariableCounter = 0;
 		}
 		// now I have to get all of the records
 		for (int i = 3; i < lines.size(); i++) {
@@ -154,46 +216,69 @@ public class DecisionTreeClassifier {
 			String label = comps[comps.length - 1];
 			for(int colIndex = 0; colIndex < comps.length - 1; colIndex++){
 				String stringValAtColIndex = comps[colIndex];
-				String typeOfAttr = attributeList.get(colIndex);
-				switch (typeOfAttr) {
-				case ORDINAL:
-					HashMap<String, Double> levelOfOrdinalToDoubleAmount = valsForOrdinalVarAtColumn.get(colIndex);
-					double dub = levelOfOrdinalToDoubleAmount.get(stringValAtColIndex);
-					attrs[colIndex] = dub;
-					break;
-				case CONTINUOUS://will have to normalize after
-					double amountAtColIndex = Double.parseDouble(stringValAtColIndex);
-					attrs[colIndex] = amountAtColIndex;
-					break;
-				case BINARY:
-					attrs[colIndex] = (double)binaryNameToIntSymbol.get(stringValAtColIndex);
-					break;
-				case CATEGORICAL:
-					attrs[colIndex] = (double)categoricalNameToIntSymbol.get(stringValAtColIndex);
-					break;
-				}
+				attrs[colIndex] = (double)binaryNameToIntSymbol.get(stringValAtColIndex);
 			}
 			Record recordToAdd = new Record(attrs, label);
 			records.add(recordToAdd);
 		}
-		//normalizing continuous variables in records so that they range from 0 to 1
-		for(Record record: records){
-			for(int index = 0; index < record.attrList.length;index++){
-				if(attributeList.get(index).equals(CONTINUOUS)){
-					double[] rangeAtCol = rangeAtColumn.get(index);
-					double max = rangeAtCol[1];
-					double min = rangeAtCol[0];
-					record.attrList[index] = (record.attrList[index] - min) / (max - min);
-				}
+	}// end of loadTrainingData()
+	/*
+	public ArrayList<Record> loadTestRecordsFromFile(String fileName) throws IOException{
+		String whitespace = "[ ]+";
+		List<String> lines = Files.readAllLines(Paths.get(fileName),
+				Charset.defaultCharset());
+		ArrayList<Record> testRecords = 
+		return null;
+	}
+	*/
+	private Integer bestColIndexToSplitRecords(Set<Integer> indicesOfRecords, Set<Integer> remainingColIndices){
+		if(remainingColIndices.size() == 0 || indicesOfRecords.size() == 0){
+			return null;
+		}
+		double minValue = Double.MAX_VALUE;
+		Integer bestColumnIndex = null;//the column index has the lowest average weighted entropy
+		for(Integer colIndex: remainingColIndices){
+			double value = averageWeightedEntropy(indicesOfRecords, colIndex);
+			if (value < minValue){
+				minValue = value;
+				bestColumnIndex = colIndex;
 			}
 		}
-	}// end of loadTrainingData()
+		return bestColumnIndex;
+	}
 	
-	private HashMap<Integer, Integer> frequenciesOfBinaryValues(TreeSet<Integer> indicesOfRecords, String colName){
-		int indexOfColumn = attributeList.indexOf(colName);
+	private double averageWeightedEntropy(Set<Integer> indicesOfRecords, int colIndex){
+		
+		TreeSet<Integer> leftRecords = indicesOfRecordsWithValueAtColumn(indicesOfRecords, 0, colIndex);
+		HashMap<Integer, Integer> freqOfBinValuesLeftRecords = frequenciesOfBinaryValues(leftRecords, colIndex);
+		TreeSet<Integer> rightRecords = indicesOfRecordsWithValueAtColumn(indicesOfRecords, 1, colIndex);
+		HashMap<Integer, Integer> freqOfBinValuesRightRecords = frequenciesOfBinaryValues(rightRecords, colIndex);
+		
+		double entropyLeft = entropy(freqOfBinValuesLeftRecords, leftRecords.size());
+		double entropyRight = entropy(freqOfBinValuesRightRecords, rightRecords.size());
+		
+		double average = entropyLeft*leftRecords.size() / indicesOfRecords.size() +
+				entropyRight * rightRecords.size() / indicesOfRecords.size();
+		
+		return average;
+	}
+	
+	private TreeSet<Integer> indicesOfRecordsWithValueAtColumn(Set<Integer> indicesOfRecords,
+																	int value, int colIndex){
+		TreeSet<Integer> setOfIndicesToReturn = new TreeSet<>();
+		for(Integer index: indicesOfRecords){
+			Record recordAtRow = records.get(index);
+			if((int)recordAtRow.attrList[colIndex] == value){
+				setOfIndicesToReturn.add(index);
+			}
+		}
+		return setOfIndicesToReturn;
+	}
+	
+	private HashMap<Integer, Integer> frequenciesOfBinaryValues(TreeSet<Integer> indicesOfRecords, int colIndex){
 		HashMap<Integer, Integer> frequencyOfBinaryValues = new HashMap<>();
-		for(Integer colIndex: indicesOfRecords){
-			Record record = records.get(colIndex);
+		for(Integer rowIndex: indicesOfRecords){
+			Record record = records.get(rowIndex);
 			int key = (int)record.attrList[colIndex];
 			if(frequencyOfBinaryValues.containsKey(key)){
 				frequencyOfBinaryValues.put(key, frequencyOfBinaryValues.get(key) + 1);
@@ -232,6 +317,33 @@ public class DecisionTreeClassifier {
 		return 1 - maxClassError;
 	}
 	
+	public double computeTrainingError(){
+		int numberOfMisclassifiedRecords = 0;
+		for(Record currentRecord: records){
+			String labelOfClassifiedRecord = classify(currentRecord);
+			System.out.println(currentRecord.label);
+			System.out.println(labelOfClassifiedRecord);
+			System.out.println();
+			if(currentRecord.label.equals(labelOfClassifiedRecord) == false){
+				numberOfMisclassifiedRecords++;
+			}
+		}
+		return (double)numberOfMisclassifiedRecords / records.size();
+	}
+	
+	public String classify(Record recordToClassify){
+		TreeNode currentNode = root;
+		while(currentNode.nodeType.equals(TreeNode.INTERNAL)){
+			int colIndexForSplitting = currentNode.attribute;
+			if(recordToClassify.attrList[colIndexForSplitting] == RIGHT_RECORDS_BINARY_VALUE){
+				currentNode = currentNode.right;
+			}else{
+				currentNode = currentNode.left;
+			}
+		}
+		return currentNode.labelName;
+	}
+	
 	public static void zTestClassErrors(){
 		HashMap<Integer, Integer> map = new HashMap<>();
 		map.put(0, 16);
@@ -246,17 +358,29 @@ public class DecisionTreeClassifier {
 		System.out.println("class error: " + classError);
 	}
 	
+	public String treeString(TreeNode node, int hierarchy){
+		if(node == null){
+			return "";
+		}else{
+			StringBuffer sBuffer = new StringBuffer("");
+			for(int i = 0; i < hierarchy; i++){
+				sBuffer.append("\t");
+			}
+			sBuffer.append(node.toString() + "\n");
+			sBuffer.append(treeString(node.left, hierarchy + 1));
+			sBuffer.append(treeString(node.right, hierarchy + 1));
+			return sBuffer.toString();
+		}
+	}
+
 	@Override
 	public String toString() {
 		StringBuffer sBuffer = new StringBuffer("");
 		sBuffer.append("# Of Attributes: " + this.numberOfAttributes + "\n");
 		sBuffer.append("# Of Classes: " + this.numberOfClasses + "\n");
-		sBuffer.append("# Of Records: " + this.numberOfRecords + "\n");
-		// can add # of nearest neighbors, etc
-		for (Record record : this.records) {
-			sBuffer.append(record.toString() + "\n");
-		}
-		sBuffer.deleteCharAt(sBuffer.length() - 1);
+		sBuffer.append("# Of Records: " + this.numberOfRecords + "\n");		
+		String treeDescription = treeString(root, 0);
+		sBuffer.append(treeDescription);
 		return sBuffer.toString();
 	}
 
