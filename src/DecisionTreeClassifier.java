befcf2a176008e1363bab1d0bd372e68e4cc543c
Will <wgilles1@emich.edu>
2016-02-27 19:12:21 -0500

    Fixed a bug in the Decision Tree.  Now it correctly builds the tree.  There are still problems with the one out validation error (too high).

--- src/DecisionTreeClassifier.java
+++ src/DecisionTreeClassifier.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -78,9 +79,10 @@ public class DecisionTreeClassifier {
 		System.out.println(dtc);
 		double trainingError = dtc.computeTrainingError();
 		System.out.println("trainingError: " + trainingError);
-		//System.out.println(dtc.root);
-		//System.out.println(dtc.root.left);
-		//System.out.println(dtc.root.right);
+		double oneOutError = dtc.oneOutValidationError();
+		System.out.println("one out error: " + oneOutError);
+		double randomSamplingError = dtc.randomSamplingClassificationError();
+		System.out.println("random sampling error: " + randomSamplingError);
 	}
 	
 	private int numberOfRecords;
@@ -109,11 +111,10 @@ public class DecisionTreeClassifier {
 	}
 	
 	public TreeNode build(TreeSet<Integer> indicesOfRecordsLeft, TreeSet<Integer> remainingColIndices){
-		//System.out.println("building iteration");
 		if(areRecordsSameClass(indicesOfRecordsLeft)){
-			String labelName = records.get(0).label;
+			String labelName = records.get(indicesOfRecordsLeft.first()).label;
 			return new TreeNode(TreeNode.LEAF, labelName, -1,null, null);
-		}else if(remainingColIndices.size() == 0){
+		}else if(remainingColIndices.size() == 0 || indicesOfRecordsLeft.size() < 4){
 			String majorityLabel = majorityLabel(indicesOfRecordsLeft);
 			return new TreeNode(TreeNode.LEAF, majorityLabel, -1, null, null);
 		}else{//the real tree building
@@ -191,11 +192,7 @@ public class DecisionTreeClassifier {
 		this.numberOfAttributes = Integer.parseInt(componentsOfFirstLine[1]);
 		this.numberOfClasses = Integer.parseInt(componentsOfFirstLine[2]);
 
-		// second line reading the attribute types
-		String[] componentsOfThirdLine = lines.get(1).split(whitespace);
-		//for binary variables
 		int binaryVariableCounter = 0;
-		
 		// third line reading the ranges of the attribute types
 		// uses rangeAtColumn hash map
 		String[] listOfRanges = lines.get(2).split(whitespace);
@@ -222,15 +219,24 @@ public class DecisionTreeClassifier {
 			records.add(recordToAdd);
 		}
 	}// end of loadTrainingData()
-	/*
+	
 	public ArrayList<Record> loadTestRecordsFromFile(String fileName) throws IOException{
 		String whitespace = "[ ]+";
 		List<String> lines = Files.readAllLines(Paths.get(fileName),
 				Charset.defaultCharset());
-		ArrayList<Record> testRecords = 
-		return null;
+		ArrayList<Record> testRecords = new ArrayList<>();
+		for(String line: lines){
+			String[] comps = line.split(whitespace);
+			double[] attrs = new double[comps.length];
+			for(int i = 0; i < comps.length; i++){
+				attrs[i] = binaryNameToIntSymbol.get(comps[i]);
+			}
+			Record recordToAdd = new Record(attrs, null);
+			testRecords.add(recordToAdd);
+		}
+		return testRecords;
 	}
-	*/
+	
 	private Integer bestColIndexToSplitRecords(Set<Integer> indicesOfRecords, Set<Integer> remainingColIndices){
 		if(remainingColIndices.size() == 0 || indicesOfRecords.size() == 0){
 			return null;
@@ -249,14 +255,16 @@ public class DecisionTreeClassifier {
 	
 	private double averageWeightedEntropy(Set<Integer> indicesOfRecords, int colIndex){
 		
-		TreeSet<Integer> leftRecords = indicesOfRecordsWithValueAtColumn(indicesOfRecords, 0, colIndex);
-		HashMap<Integer, Integer> freqOfBinValuesLeftRecords = frequenciesOfBinaryValues(leftRecords, colIndex);
-		TreeSet<Integer> rightRecords = indicesOfRecordsWithValueAtColumn(indicesOfRecords, 1, colIndex);
-		HashMap<Integer, Integer> freqOfBinValuesRightRecords = frequenciesOfBinaryValues(rightRecords, colIndex);
-		
-		double entropyLeft = entropy(freqOfBinValuesLeftRecords, leftRecords.size());
-		double entropyRight = entropy(freqOfBinValuesRightRecords, rightRecords.size());
-		
+		TreeSet<Integer> leftRecords = indicesOfRecordsWithValueAtColumn(indicesOfRecords, 
+				LEFT_RECORDS_BINARY_VALUE, colIndex);
+		HashMap<String, Integer> freqOfBinValuesLeftRecords = frequenciesOfBinaryValues(leftRecords, colIndex);
+		TreeSet<Integer> rightRecords = indicesOfRecordsWithValueAtColumn(indicesOfRecords, 
+				RIGHT_RECORDS_BINARY_VALUE, colIndex);
+		HashMap<String, Integer> freqOfBinValuesRightRecords = frequenciesOfBinaryValues(rightRecords, colIndex);
+
+		double entropyLeft = gini(freqOfBinValuesLeftRecords, leftRecords.size());
+		double entropyRight = gini(freqOfBinValuesRightRecords, rightRecords.size());
+
 		double average = entropyLeft*leftRecords.size() / indicesOfRecords.size() +
 				entropyRight * rightRecords.size() / indicesOfRecords.size();
 		
@@ -275,60 +283,46 @@ public class DecisionTreeClassifier {
 		return setOfIndicesToReturn;
 	}
 	
-	private HashMap<Integer, Integer> frequenciesOfBinaryValues(TreeSet<Integer> indicesOfRecords, int colIndex){
-		HashMap<Integer, Integer> frequencyOfBinaryValues = new HashMap<>();
+	private HashMap<String, Integer> frequenciesOfBinaryValues(TreeSet<Integer> indicesOfRecords, int colIndex){
+		HashMap<String, Integer> frequencyOfBinaryValues = new HashMap<>();
 		for(Integer rowIndex: indicesOfRecords){
 			Record record = records.get(rowIndex);
-			int key = (int)record.attrList[colIndex];
-			if(frequencyOfBinaryValues.containsKey(key)){
-				frequencyOfBinaryValues.put(key, frequencyOfBinaryValues.get(key) + 1);
+			String labelKey = record.label;
+			if(frequencyOfBinaryValues.containsKey(labelKey)){
+				frequencyOfBinaryValues.put(labelKey, frequencyOfBinaryValues.get(labelKey) + 1);
 			}else{//if the key is not in the hashmap yet
-				frequencyOfBinaryValues.put(key, 1);
+				frequencyOfBinaryValues.put(labelKey, 1);
 			}
 		}
 		return frequencyOfBinaryValues;
 	}
 	
-	private static double entropy(HashMap<Integer, Integer> frequencyOfBinaryValues, int size){
+	private static double entropy(HashMap<String, Integer> frequencyOfBinaryValues, int size){
 		double entropySoFar = 0.0;
-		for(Integer binaryKey: frequencyOfBinaryValues.keySet()){
+		for(String binaryKey: frequencyOfBinaryValues.keySet()){
 			double ratio = (double)frequencyOfBinaryValues.get(binaryKey) / size;
 			entropySoFar += ratio * Math.log(ratio);
 		}
 		return -entropySoFar;//negative of entropySoFar
 	}
 	
-	private static double gini(HashMap<Integer, Integer> frequencyOfBinaryValues, int size){
+	private static double gini(HashMap<String, Integer> frequencyOfBinaryValues, int size){
 		double giniFigureSoFar = 0.0;
-		for(Integer binaryKey: frequencyOfBinaryValues.keySet()){
+		for(String binaryKey: frequencyOfBinaryValues.keySet()){
 			giniFigureSoFar += Math.pow((double)frequencyOfBinaryValues.get(binaryKey) / size, 2);
 		}
 		return 1 - giniFigureSoFar;
 	}
 	
-	private static double classError(HashMap<Integer, Integer> frequencyOfBinaryValues, int size){
+	private static double classError(HashMap<String, Integer> frequencyOfBinaryValues, int size){
 		double maxClassError = Double.MIN_VALUE;
-		for (Integer binaryKey : frequencyOfBinaryValues.keySet()) {
+		for (String binaryKey : frequencyOfBinaryValues.keySet()) {
 			double ratio = (double)frequencyOfBinaryValues.get(binaryKey) / size;
 			if(ratio > maxClassError){
 				maxClassError = ratio;
 			}
 		}
 		return 1 - maxClassError;
-	}
-	
-	public double computeTrainingError(){
-		int numberOfMisclassifiedRecords = 0;
-		for(Record currentRecord: records){
-			String labelOfClassifiedRecord = classify(currentRecord);
-			System.out.println(currentRecord.label);
-			System.out.println(labelOfClassifiedRecord);
-			System.out.println();
-			if(currentRecord.label.equals(labelOfClassifiedRecord) == false){
-				numberOfMisclassifiedRecords++;
-			}
-		}
-		return (double)numberOfMisclassifiedRecords / records.size();
 	}
 	
 	public String classify(Record recordToClassify){
@@ -344,10 +338,63 @@ public class DecisionTreeClassifier {
 		return currentNode.labelName;
 	}
 	
+	public double computeTrainingError(){
+		int numberOfMisclassifiedRecords = 0;
+		for(Record currentRecord: records){
+			String labelOfClassifiedRecord = classify(currentRecord);
+			if(currentRecord.label.equals(labelOfClassifiedRecord) == false){
+				numberOfMisclassifiedRecords++;
+			}
+		}
+		return (double)numberOfMisclassifiedRecords / records.size();
+	}
+	
+	public double randomSamplingClassificationError(){
+		//25% of the records will be used as test records; 75% for training.
+		Random rng = new Random();
+		int numberOfTestRecords = (int)(records.size() * 0.25);
+		int numberOfMisclassifiedRecords = 0;
+		int ITERATIONS = 10000;
+		for(int iteration = 0; iteration < ITERATIONS; iteration++){
+			ArrayList<Record> testRecords = new ArrayList<>();
+			for(int i = 0; i < numberOfTestRecords; i++){
+				int randomIndex = rng.nextInt(records.size());
+				testRecords.add(records.remove(randomIndex));
+			}
+			
+			buildTree();
+			for(Record testRecord: testRecords){
+				String label = classify(testRecord);
+				if(label.equals(testRecord.label) == false){
+					numberOfMisclassifiedRecords++;
+				}
+			}
+			
+			records.addAll(testRecords);
+		}
+		return (double)numberOfMisclassifiedRecords / (numberOfTestRecords * ITERATIONS);
+	}
+	
+	public double oneOutValidationError(){
+		int numberOfMisclassifiedRecords = 0;
+		for(int i = 0; i < records.size(); i++){
+			Record recordToClassify = records.remove(i);
+			buildTree();
+			String label = classify(recordToClassify);
+			System.out.println("classified label: " + label + ", record's label: " + recordToClassify.label);
+			if(label.equals(recordToClassify.label) == false){
+				numberOfMisclassifiedRecords++;
+			}
+			records.add(i, recordToClassify);
+		}
+		System.out.println("size: " + records.size());
+		return (double)numberOfMisclassifiedRecords / records.size();
+	}
+	
 	public static void zTestClassErrors(){
-		HashMap<Integer, Integer> map = new HashMap<>();
-		map.put(0, 16);
-		map.put(1, 6);
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("0", 16);
+		map.put("1", 6);
 		int size = 20;
 		
 		double entropy = DecisionTreeClassifier.entropy(map, size);
